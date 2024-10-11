51820f76f7eea686a519e11a55bbbde287c25b88
José Ignacio Amelivia Santiago <hello@namelivia.com>
2024-10-11 23:09:27 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "hcloud_ssh_key" "deployer" {
+  name   = "${var.server_name}-key"
+  public_key = "${var.ssh_key}"
+
+}
+
+resource "hcloud_server" "server" {
+  name        = "${var.server_name}"
+  image       = "ubuntu-24.04"
+  data_center = "fsn1-dc8"
+  server_type = "cx22"
+  ssh_keys    = [hcloud_ssh_key.deployer.id]
+  public_net {
+    ipv4_enabled = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ssh_key" {
+  type = string
+  description = "SSH Key to access the server"
+}
+
+variable "server_name" {
+  type = string
+  description = "Name for the server"
+}
